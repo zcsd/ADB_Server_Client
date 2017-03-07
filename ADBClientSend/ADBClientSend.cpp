9d32a92717e836ff784218589a80e75fed4c3af2
zcsd <zichun92@gmail.com>
2017-03-07 23:50:49 +0800

    can send continuous image in low speed

--- ADBClientSend/ADBClientSend.cpp
+++ ADBClientSend/ADBClientSend.cpp
@@ -1,12 +1,12 @@
 #include <WINSOCK2.H>    
 #include <STDIO.H>    
 #include "windows.h"
-#include <iostream>
-#include <conio.h>    
+#include<iostream>
+#include<conio.h>    
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
        
 #pragma  comment(lib,"ws2_32.lib")    
 #define SERVER_IP "127.0.0.1"
@@ -14,20 +14,6 @@
 
 int main(int argc, char* argv[])
 {
-	cv::Mat frame;
-	frame = cv::imread("123.jpg");
-
-	if (frame.empty()) {
-		printf("Load Image Error\n");
-		return 0;
-	}
-
-	int imgSize = frame.total()*frame.elemSize();
-	int bytes = 0;
-
-	printf("width is %d, height is %d.\n", frame.cols, frame.rows);
-	printf("Image size is %d.\n", imgSize);
-
 	WORD sockVersion = MAKEWORD(2, 2);
 	WSADATA data;
 	if (WSAStartup(sockVersion, &data) != 0)
@@ -59,20 +45,51 @@ int main(int argc, char* argv[])
 	printf("Please input your choice:");
 	scanf("%d", &input);
 
-	frame = (frame.reshape(0, 1));
+	cv::VideoCapture capWebcam(0);            // declare a VideoCapture object and associate to webcam, 0 => use 1st webcam
+	capWebcam.set(CV_CAP_PROP_FRAME_WIDTH, 250);
+	capWebcam.set(CV_CAP_PROP_FRAME_HEIGHT, 250);
+	capWebcam.set(CV_CAP_PROP_FPS, 2);
+	if (capWebcam.isOpened() == false) {                                // check if VideoCapture object was associated to webcam successfully
+		std::cout << "error: capWebcam not accessed successfully\n\n";      // if not, print error message to std out
+		_getch();                                                           // may have to modify this line if not using Windows
+		return(0);                                                           // and exit program
+	}
+	cv::Mat frame;        // input image
+	char charCheckForEscKey = 0;
 
-	const char* data0 = (const char *)frame.data;
-	if ((bytes = send(sclient, data0, imgSize, 0)) < 0) {
-		printf("Send Fail\n");
-		return 0;
+	while (charCheckForEscKey != 27 && capWebcam.isOpened()) {
+		bool blnFrameReadSuccessfully = capWebcam.read(frame);            // get next frame
+
+		if (!blnFrameReadSuccessfully || frame.empty()) {                 // if frame not read successfully
+			printf("error: frame not read from webcam\n");                 // print error message to std out
+			break;                                                              // and jump out of while loop
+		}
+
+		cv::namedWindow("frameWnd", CV_WINDOW_NORMAL);          // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image															// CV_WINDOW_AUTOSIZE is the default
+		cv::imshow("frameWnd", frame);
+
+		int imgSize = frame.total()*frame.elemSize();
+		int bytes = 0;
+
+		frame = (frame.reshape(0, 1));
+
+		const char* data0 = (const char *)frame.data;
+		if ((bytes = send(sclient, data0, imgSize, 0)) < 0) {
+			printf("Send Fail\n");
+			return 0;
+		}
+
+		if (bytes != imgSize) {
+			printf("Connection Closed\n");
+			closesocket(sclient);
+			return 0;
+		}
+
+		charCheckForEscKey = cv::waitKey(100);
 	}
 
-	if (bytes != imgSize) {
-		printf("Connection Closed\n");
-		closesocket(sclient);
-		return 0;
-	}
-	printf("Sent Alraeady\n");
+	printf("Sent Finished!\n");
+
 /*
 	bool isContinue = true;
 	while (isContinue) {
--- ADBServerShow/app/src/main/java/com/ntu/zc/adbservershow/MainActivity.java
+++ ADBServerShow/app/src/main/java/com/ntu/zc/adbservershow/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
@@ -18,10 +17,8 @@ import java.net.Socket;
 public class MainActivity extends Activity {
 
     private final int SERVER_PORT = 9000;
-    private TextView textView;
     private ImageView imageView;
-    private String content = "";
-    public static byte imageByte[];
+    public byte imageByte[];
     public int imageSize = 187500;
 
     boolean flag =true;
@@ -29,7 +26,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //textView = (TextView)findViewById(R.id.tv);
         imageView = (ImageView)findViewById(R.id.imageView);
 
         new Thread() {
@@ -41,7 +37,6 @@ public class MainActivity extends Activity {
 
     private void startServer() {
         try {
-
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
 
             while (true) {
@@ -50,7 +45,7 @@ public class MainActivity extends Activity {
                     @Override
                     public void run() {
                         try {
-                            Log.e("hehheh", "Someone come:");
+                            Log.e("hehe", "Someone come:");
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -58,69 +53,66 @@ public class MainActivity extends Activity {
                                 }
                             });
 
-                            InputStream in  = client.getInputStream();
+                            InputStream in = client.getInputStream();
 
-                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            while(true) {
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                byte buffer[] = new byte[1024];
+                                int remainingBytes = imageSize;
 
-                            byte buffer[] = new byte[1024];
-                            int remainingBytes = imageSize;
-                            while(remainingBytes > 0){
-                                int bytesRead = in.read(buffer);
+                                while (remainingBytes > 0) {
+                                    int bytesRead = in.read(buffer);
 
-                                if(bytesRead < 0){
-                                    throw new IOException("Unexpected end of data");
+                                    if (bytesRead < 0) {
+                                        throw new IOException("Unexpected end of data");
+                                    }
+                                    baos.write(buffer, 0, bytesRead);
+                                    remainingBytes -= bytesRead;
                                 }
-                                baos.write(buffer, 0, bytesRead);
-                                remainingBytes -= bytesRead;
+
+                                //in.close();
+                                imageByte = baos.toByteArray();
+                                baos.close();
+
+                                int nrOfPixels = 250 * 250;
+                                int pixels[] = new int[nrOfPixels];
+                                for (int i = 0; i < nrOfPixels; i++) {
+                                    int r = imageByte[3 * i];
+                                    int g = imageByte[3 * i + 1];
+                                    int b = imageByte[3 * i + 2];
+
+                                    if (r < 0) r = r + 256;
+                                    if (g < 0) g = g + 256;
+                                    if (b < 0) b = b + 256;
+                                    pixels[i] = Color.rgb(b, g, r);
+                                }
+                                final Bitmap bitmap = Bitmap.createBitmap(pixels, 250, 250, Bitmap.Config.ARGB_8888);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        imageView.setImageBitmap(bitmap);
+                                    }
+                                });
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(MainActivity.this, "Received!!", 1).show();
+                                    }
+                                });
                             }
-
-                            in.close();
-
-                            imageByte = baos.toByteArray();
-                            baos.close();
-
-                            int nrOfPixels = 250 * 250;
-                            int pixels[] = new int[nrOfPixels];
-                            for(int i = 0; i < nrOfPixels; i++){
-                                int r = imageByte[3*i];
-                                int g = imageByte[3*i +1];
-                                int b = imageByte[3*i +2];
-
-                                if(r < 0)   r = r + 256;
-                                if(g < 0)   g = g + 256;
-                                if(b < 0)   b = b + 256;
-                                pixels[i] = Color.rgb(b, g, r);
-                            }
-                            final Bitmap bitmap = Bitmap.createBitmap(pixels, 250, 250, Bitmap.Config.ARGB_8888);
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    imageView.setImageBitmap(bitmap);
-                                }
-                            });
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(MainActivity.this, "Received!!!!", 1).show();
-                                }
-                            });
-
                         } catch (Exception e) {
                             e.printStackTrace();
                         } finally {
                             try {
                                 client.close();
                             } catch (IOException e) {
-                                // TODO Auto-generated catch block
                                 e.printStackTrace();
                             }
                         }
                         try {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
-                            // TODO Auto-generated catch block
                             e.printStackTrace();
                         }
                     }
